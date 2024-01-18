519b577e853a7c99a4426eec5cbcb8e0680fb352
nzyoni <nzyoni@gmail.com>
2024-01-18 20:15:52 +0200

    Remove server side fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { BookCatalog } from "@/components/BookCatalog";
+import { BookSearchResult } from "@/components/types";
 import { booksApiService } from "@/services/books.api.service";
 import React from "react";
 
 const initialParams = { page: 1, pageSize: 10 };
 
 export default async function Home() {
-  const results = await booksApiService.searchBooks(initialParams);
+  // const results = await booksApiService.searchBooks(initialParams);
+  const results = {} as BookSearchResult;
 
   return (
     <main style={{ display: "flex", justifyContent: "center" }}>
