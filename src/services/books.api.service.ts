a488903d3ccf31fc4dc83db6c0fbb48bf64b0c9c
nzyoni <nzyoni@gmail.com>
2024-01-18 21:05:59 +0200

    remoev usage of .env

--- src/services/books.api.service.ts
+++ src/services/books.api.service.ts
@@ -1,13 +1,11 @@
 import { SearchRequest } from "@/app/api/books/utils";
 import { BookSearchResult } from "@/components/types";
 
-const hostUrl = process.env.URL ?? "";
-
 const searchBooks = async (
   options: SearchRequest
 ): Promise<BookSearchResult> => {
   try {
-    const response = await fetch(`${hostUrl}/api/books`, {
+    const response = await fetch(`/api/books`, {
       method: "post",
       body: JSON.stringify(options),
     });
